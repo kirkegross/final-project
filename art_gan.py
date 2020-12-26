64004eeda5e431bb865c3166fb37e768cc361b16
kirkegross <kirke.gross@gmail.com>
2020-12-26 21:17:18 +0200

    fixed the code

--- art_gan.py
+++ art_gan.py
@@ -105,20 +105,35 @@ def save_images(cnt, noise):
     
 image_shape = (IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
 optimizer = Adam(1.5e-4, 0.5)
+#calling our build_discriminator function and passing the image shape
 discriminator = build_discriminator(image_shape)
+
+#compiling it with a loss function and an optimizer
 discriminator.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+
+#calling our build_generator function and passing our random_input noise vector as its input
 generator = build_generator(NOISE_SIZE, IMAGE_CHANNELS)
 random_input = Input(shape=(NOISE_SIZE,))
 generated_image = generator(random_input)
+
+#preventing our discriminator from training
 discriminator.trainable = False
 validity = discriminator(generated_image)
 combined = Model(random_input, validity)
 combined.compile(loss="binary_crossentropy",
 optimizer=optimizer, metrics=["accuracy"])
+
+#defining two vectors as y_real and y_fake
+#These vectors are composed of random 0’s and 1’s values.
 y_real = np.ones((BATCH_SIZE, 1))
 y_fake = np.zeros((BATCH_SIZE, 1))
+
+#fixed_noise/output_images: this will result in generating images that are saved later on which we can see it getting better on every iteration
 fixed_noise = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, NOISE_SIZE))
 cnt = 1
+
+#After that, we are going to iterate over our training data with the range of epochs we’ve defined.
+#During the iteration process, we are taking a sample from a real image and putting that on x_real. After that, we are defining a noise vector and passing that to our generator model to generate a fake image in x_fake.
 for epoch in range(EPOCHS):
  idx = np.random.randint(0, training_data.shape[0], BATCH_SIZE)
  x_real = training_data[idx]
@@ -126,12 +141,18 @@ for epoch in range(EPOCHS):
  noise= np.random.normal(0, 1, (BATCH_SIZE, NOISE_SIZE))
  x_fake = generator.predict(noise)
  
+    
+#Then we are training our discriminator model in both real and fake images separately.
  discriminator_metric_real = discriminator.train_on_batch(x_real, y_real)
 discriminator_metric_generated = discriminator.train_on_batch(
  x_fake, y_fake)
  
+#After training, we are taking the metric from both models and taking the average.
 discriminator_metric = 0.5 * np.add(discriminator_metric_real, discriminator_metric_generated)
 generator_metric = combined.train_on_batch(noise, y_real)
+
+#in the end we have an if statement which checks for our checkpoint
+#If it reaches the checkpoint then it saves the current iteration noise and prints the current accuracy of generator and discriminator
 if epoch % SAVE_FREQ == 0:
    save_images(cnt, fixed_noise)
    cnt += 1
@@ -139,56 +160,3 @@ if epoch % SAVE_FREQ == 0:
    print(f"{epoch} epoch, Discriminator accuracy: {100*  discriminator_metric[1]}, Generator accuracy: {100 * generator_metric[1]}")
     
     
-image_shape = (IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS)
-optimizer = Adam(1.5e-4, 0.5)
-discriminator = build_discriminator(image_shape)
-discriminator.compile(loss="binary_crossentropy",
-optimizer=optimizer, metrics=["accuracy"])
-
-generator = build_generator(NOISE_SIZE, IMAGE_CHANNELS)
-random_input = Input(shape=(NOISE_SIZE,))
-generated_image = generator(random_input)
-
-discriminator.trainable = False
-validity = discriminator(generated_image)
-combined = Model(random_input, validity)
-combined.compile(loss="binary_crossentropy",
-optimizer=optimizer, metrics=["accuracy"])
-
-y_real = np.ones((BATCH_SIZE, 1))
-y_fake = np.zeros((BATCH_SIZE, 1))
-fixed_noise = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, NOISE_SIZE))
-
-cnt = 1
-for epoch in range(EPOCHS):
-  idx = np.random.randint(0, training_data.shape[0], BATCH_SIZE)
-  x_real = training_data[idx]
- 
-  noise= np.random.normal(0, 1, (BATCH_SIZE, NOISE_SIZE))
-  x_fake = generator.predict(noise)
- 
-  discriminator_metric_real = discriminator.train_on_batch(x_real, y_real)
-  discriminator_metric_generated = discriminator.train_on_batch(
- x_fake, y_fake)
- 
-  discriminator_metric = 0.5 * np.add(discriminator_metric_real,   discriminator_metric_generated)
-
-  generator_metric = combined.train_on_batch(noise, y_real)
-  if epoch % SAVE_FREQ == 0:
-     save_images(cnt, fixed_noise)
-     cnt += 1
- 
-     print(f"{epoch} epoch, Discriminator accuracy: {100*  discriminator_metric[1]}, Generator accuracy: {100 * generator_metric[1]}")
-    
-    
-discriminator_metric_real = discriminator.train_on_batch(x_real, y_real)
-discriminator_metric_generated = discriminator.train_on_batch(
- x_fake, y_fake)
-
-discriminator_metric = 0.5 * np.add(discriminator_metric_real, discriminator_metric_generated)
-
-if epoch % SAVE_FREQ == 0:
- save_images(cnt, fixed_noise)
- cnt += 1
- 
- print(f"{epoch} epoch, Discriminator accuracy: {100* discriminator_metric[1]}, Generator accuracy: {100 * generator_metric[1]}")
